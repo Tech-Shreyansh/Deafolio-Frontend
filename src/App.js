65ea352a8af911ebfe62c0b476562e776b64a00f
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2023-01-07 11:59:02 +0530

    added illustration and login box

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import LoginApp from './Auth/Login/login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <LoginApp /> ;
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/login" element={<LoginApp />} ></Route>
+    {/* <Route path="/signup" element={<SignUp />}></Route>
+    <Route path="/authphone" element={<Phone />}></Route>
+    <Route path="/otp" element={<OTP />}></Route>
+    <Route path="/phoneotp" element={<PhoneOTP />}></Route>
+    <Route path="/forgot_password" element={<Forgot />}></Route>
+    <Route path="/reset_password" element={<Resetpass />}></Route>
+    <Route path="/set_password" element={<Setpass />}></Route> */}
+  </Routes>
+</BrowserRouter>;
 }
 
 export default App;
--- src/Auth/Login/login.jsx
+++ src/Auth/Login/login.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Logo from "../components/logo";
+import illustration from "../images/illustration.svg";
+import "./login.css"
 
 const LoginApp = () => {
     return<div>
         <Logo />
+        <img id="illustration" src={illustration} alt="Deaf winning the World" />
+        <div id="login">
+            
+        </div>
     </div>
 }
 
