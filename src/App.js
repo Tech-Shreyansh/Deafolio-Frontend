79f1d97c4fee88cdad063864ab043a83cb1c35b3
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2023-01-07 14:21:39 +0530

    added signup page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import LoginApp from './Auth/Login/login';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignupApp from './Auth/signup/sign';
+import ForgotApp from './Auth/forgot_password/forgot';
 
 function App() {
   return <BrowserRouter>
   <Routes>
     <Route path="/login" element={<LoginApp />} ></Route>
-    {/* <Route path="/signup" element={<SignUp />}></Route>
-    <Route path="/authphone" element={<Phone />}></Route>
+    <Route path="/signup" element={<SignupApp />}></Route>
+    <Route path="/forgot_password" element={<ForgotApp />}></Route>
+    {/* <Route path="/authphone" element={<Phone />}></Route>
     <Route path="/otp" element={<OTP />}></Route>
     <Route path="/phoneotp" element={<PhoneOTP />}></Route>
-    <Route path="/forgot_password" element={<Forgot />}></Route>
     <Route path="/reset_password" element={<Resetpass />}></Route>
     <Route path="/set_password" element={<Setpass />}></Route> */}
   </Routes>
